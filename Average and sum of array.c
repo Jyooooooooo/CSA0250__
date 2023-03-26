cd334c3094532cb8a0e2e7108996726434912c34
Jyooooooooo <128606586+Jyooooooooo@users.noreply.github.com>
2023-03-26 18:11:37 +0530

    Add files via upload

---
+++ Average and sum of array.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int marks[10],i,n,sum = 0;
+	double average;
+	printf("Enter number of elements:");
+	scanf("%d", &n);
+	for(i=0;i<n;i++)
+	{
+		printf("Enter number%d:",i+1);
+		scanf("%d",&marks[i]);
+		sum +=marks[i];
+	}
+	average =(double)sum/n;
+	printf("Sum = %.2f",(double)sum);
+	printf("Average =%.2f",average);
+	return 0;
+	
+}
