96b3ab05f966876018972e62f278551d112d2617
Jyooooooooo <128606586+Jyooooooooo@users.noreply.github.com>
2023-04-26 18:26:37 +0530

    Add files via upload

--- One Dimensional Array.c
+++ One Dimensional Array.c
@@ -2,14 +2,14 @@
 int main()
 {
 	int a[5], i;
-	printf("Enter Array Elements:\n");
+	printf("Enter Array Elements:/n");
 	for (i=0;i<5;i++)
 	{
 		scanf("%d",&a[i]);
 	}
-	printf("Enter Array Elements are:\n");
+	printf("Enter Array Elements are:");
 	for (i=0;i<5;i++)
 	{
-		printf("%d\n",a[i]);
+		printf("%d",a[i]);
 	}
 }
